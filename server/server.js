48438a0a310248141d4ea93d187cffeed86278a5
AanandBhandari <aanandbhandari143@gmail.com>
2018-06-05 09:03:22 +0545

    patch:id(update)

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var {Todo} = require('./models/todos.js');
 var {objectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('lodash');
 var app = express();
 app.use(bodyParser.json());
 app.post('/todo', (req,res) => {
@@ -26,21 +27,64 @@ app.get('/todo', (req,res) => {
     }, (e) => {
         res.status(400).send({e});
     });
-app.get('/todo:id',(req,res) => {
+
+});
+app.get('/todos/:id',(req,res) => {
     var id = req.params.id;
-    if (!objectID.isvalid(id)) {
-        return res.status(404).send();
-        Todo.findById(id).then((doc) => {
-            if (!doc) {
-                res.status(400).send();
-            }
-            res.send({doc});
-        }).catch((e) => {
+    
+    // if (!objectID.isValid(id)) {
+    //     return res.status(404).send();
+       
+    // }
+    Todo.findById(id).then((doc) => {
+        if (!doc) {
             res.status(400).send();
-        })
-    }
+        }
+        res.send({doc});
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
+app.delete('/todos/:id',(req,res) => {
+    let id = req.params.id;
+    
+    // if (!objectID.isValid(id)) {
+    //     return res.status(404).send();
+       
+    // }
+    Todo.findByIdAndRemove(id).then((doc) => {
+        if (!doc) {
+            res.status(400).send();
+        }
+        res.send({doc});
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
+// app.patch('/todos/:id', (req,res) => {
+//     let id = req.params.id;
+//    // if (!objectID.isValid(id)) {
+//     //    return res.status(404).send();
+       
+//     // }
+//     let body = _.pick(req.body,['text','completed']);
+//     if (_.isBoolean(body.completed) && body.completed) {
+//         body.completedAt = new Date().getTime();
+//     } else {
+//         body.completed = false;
+//         body.completedAt = null;
+//     }
+//     Todo.findByIdAndUpdate(id,{$set: body},{new : true}).then((doc) => {
+//         if (!doc) {
+//             return res.status(404).send(); 
+//         }
+//         res.send({doc});
+//     }).catch((e) => {
+//         res.status(400).send();
+//     });
+// });
 app.listen(3000, () => {
     console.log('connected to the server');
 });
+
+
