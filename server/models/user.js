292b017504dd7448a1005370874511a0566fca11
AanandBhandari <aanandbhandari143@gmail.com>
2018-08-09 16:41:50 +0545

    hashed user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 var userSchema = new mongoose.Schema({
     
         email : {
@@ -66,6 +67,19 @@ userSchema.statics.findByToken = function (token) {
     'tokens.access': 'auth'
   });
 };
+userSchema.pre('save',function (next) {
+   let user = this;
+   if (user.isModified('password')) {
+       bcrypt.genSalt(10,(err,salt) => {
+           bcrypt.hash(user.password,salt,(err,hash)=> {
+            user.password = hash;
+            next();
+           });
+       });
+   } else {
+       next();
+   } 
+});
 var User = mongoose.model('User',userSchema);
 // var newUser = new User({
 //     email : 'aanand@gmail.com '
